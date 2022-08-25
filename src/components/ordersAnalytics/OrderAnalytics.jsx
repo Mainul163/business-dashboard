5684b1e500e333b61e0eb62d5c6a7a37236d2817
mainul163 <mainulisraf163@gmail.com>
2022-08-25 17:31:01 +0600

    line chart add

--- src/components/ordersAnalytics/OrderAnalytics.jsx
+++ src/components/ordersAnalytics/OrderAnalytics.jsx
@@ -1,12 +1,193 @@
 import React from "react";
 import { Line } from "@ant-design/plots";
+import { Row, Col, Form, Button, Select, Input, DatePicker } from 'antd';
 const OrderAnalytics = () => {
-  const data = [500, 1000, 3000, 50, 20, 7851];
+  const data = [{
+    "name": "United Kingdom",
+    "year": "2001",
+    "gdp": 1640246149417.01
+  },
+  {
+    "name": "United Kingdom",
+    "year": "2002",
+    "gdp": 1784473920863.31
+  },
+  {
+    "name": "United Kingdom",
+    "year": "2003",
+    "gdp": 2053018775510.2
+  },
+  {
+    "name": "United Kingdom",
+    "year": "2004",
+    "gdp": 2416931526913.22
+  },
+  {
+    "name": "United States",
+    "year": "2009",
+    "gdp": 14448933025000
+  },
+  {
+    "name": "United States",
+    "year": "2010",
+    "gdp": 14992052727000
+  },
+  {
+    "name": "United States",
+    "year": "2011",
+    "gdp": 15542581104000
+  },
+  {
+    "name": "United States",
+    "year": "2012",
+    "gdp": 16197007349000
+  },
+  {
+    "name": "United States",
+    "year": "2013",
+    "gdp": 16784849190000
+  },
+  {
+    "name": "United States",
+    "year": "2014",
+    "gdp": 17521746534000
+  },
+  {
+    "name": "United States",
+    "year": "2015",
+    "gdp": 18219297584000
+  },
+  {
+    "name": "United States",
+    "year": "2016",
+    "gdp": 18707188235000
+  },
+  {
+    "name": "United States",
+    "year": "2017",
+    "gdp": 19485393853000
+  },
+  {
+    "name": "United States",
+    "year": "2018",
+    "gdp": 20544343456936.5
+  },
+  {
+    "name": "United Kingdom",
+    "year": "2000",
+    "gdp": 1657816613708.58
+  },];
+  const config = {
+    data,
+    xField: 'year',
+    yField: 'gdp',
+    seriesField: 'name',
+    yAxis: {
+      label: {
+        formatter: (v) => `${(v / 10e8).toFixed(1)} B`,
+      },
+    },
+    legend: {
+      position: 'top',
+    },
+    smooth: true,
+    // @TODO 后续会换一种动画方式
+    animation: {
+      appear: {
+        animation: 'path-in',
+        duration: 5000,
+      },
+    },
+  };
+  const layout = {
+    labelCol: {
+      xs: { span: 20 },
+      sm: { span: 4 },
+      md: { span: 3 },
+      lg: { span: 6 },
+      xl: { span: 8 },
+      xxl: { span: 8 }
+
+    },
+    wrapperCol: {
+      md: { span: 16 },
+      lg: { span: 16 },
+
+      span: 16,
+    },
+  };
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 4,
+      },
+      md: {
+        span: 3,
+        offset: 3,
+      },
+      lg: {
+        span: 10,
+        offset: 6,
+      },
+      xl: {
+        span: 10,
+        offset: 8,
+      },
+    },
+  };
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
     <div>
       <div>
         <h2 style={{ fontWeight: "bold" }}> Order Analytics</h2>
       </div>
+      <Row style={{ marginTop: '80px' }} gutter={[{ md: 8, lg: 8 }, 24]}>
+        <Col xs={24} sm={24} md={24} lg={24} xl={11} xxl={11}>
+          <Line {...config} />
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={11} xl={11} xxl={11} >
+       <div style={{textAlign:'center'}}>
+       <h2 style={{ fontWeight: "bold" }}> Order Analytics Information</h2>
+       </div>
+          <Form
+            {...layout}
+            onFinish={onFinish}
+            layout="horizontal"
+            initialValues={{
+              size: 'large',
+            }}
+
+            size='large'
+          >
+
+            <Form.Item label="Order Type" name='orderType' rules={[{ required: true, message: 'Please Fill Up Your Option!' }]}>
+              <Select>
+                <Select.Option value="total order">Total Order</Select.Option>
+                <Select.Option value="cancel order">Cancel Order</Select.Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item label="Amount" name='amount' rules={[{ required: true, message: 'Please Fill Up Your Input!' }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Year" name='year' rules={[{ required: true, message: 'Please Fill Up Your Input!' }]}>
+              <DatePicker picker="year" />
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 };
