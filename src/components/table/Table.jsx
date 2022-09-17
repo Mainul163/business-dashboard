efedea6261dd2e8a08ce4ba20480d1502cbdca9f
mainul163 <mainulisraf163@gmail.com>
2022-09-17 18:43:59 +0600

    pagenation working

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Table.css'
-import { Table } from 'antd';
-const TableComponent = ({data,columns,headerText}) => {
+import { Table, Pagination } from 'antd';
+const TableComponent = ({ data, columns, headerText }) => {
+    const showTotal = (total) => `Total ${total} items`;
     return (
         <div>
-           <div style={{textAlign:'center',marginBottom:'30px'}}>
-           <h2 style={{ fontWeight: "bold" }}>{headerText}</h2>
-           </div>
-         <Table dataSource={data} columns={columns}/>
+            <div style={{ textAlign: 'center', marginBottom: '30px' }}>
+                <h2 style={{ fontWeight: "bold" }}>{headerText}</h2>
+            </div>
+            <Table dataSource={data} columns={columns} pagination={false} />
+            <div className='paginationStyle'>
+                <Pagination 
+                   size="small" total={50} showTotal={()=>showTotal(20)} />
+            </div>
+
         </div>
     );
 };
